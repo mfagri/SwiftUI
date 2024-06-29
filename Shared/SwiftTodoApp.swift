672689f5323216fd07a09cdbcaa53a7b479e7269
mfagri <marouane.fagri1@gmail.com>
2024-06-29 23:00:54 +0100

    first swift app

---
+++ Shared/SwiftTodoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftTodoApp.swift
+//  Shared
+//
+//  Created by Marouane Fagri on 6/29/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftTodoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
